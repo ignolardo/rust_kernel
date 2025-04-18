0f4243d977131d8706aba08e2bf669814ee7aa88
Ignacio Rivadero <rivaderonacho@gmail.com>
2025-04-18 14:06:40 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+##### Basic kernel for x86_64 systems.
+
+This project was carried out following the ["Writing an Operating System in Rust"](https://os.phil-opp.com/) tutorial by Philipp Oppermann.
+
+This is just to improve my skills in bare metal programming and to learn about the basics of operating systems.
+
+Obviously, this kernel is not designed for use in real-world scenarios (although it could be) but to be studied to learn the fundamentals of any kernel.
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use my_os::println;
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    
+    println!("Hello World! This is my {}\n", "operating system");
+    
+    #[cfg(test)]
+    test_main();
+    
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
+
+/* #[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    my_os::test_panic_handler(info)
+} */
